f8506b9f755afac63fa92b2c812f5ee05e22fc2a
Abiodun <abiodunhodonu@gmail.com>
2022-07-27 23:29:59 +0100

    scroll button adjustment

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs'
 import { FaGithub } from 'react-icons/fa'
-import { SiBehance } from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="hhttps://www.linkedin.com/in/abiodunhodonu/" target="_blank"><BsLinkedin /></a>
         <a href="https://github.com/kingbee1" target="_blank"> <FaGithub /> </a>
-        <a href="https://behance.net" target="_blank"><SiBehance /></a>
     
     </div>
   )
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -21,7 +21,9 @@ const Services = () => {
 
             <li>
               <BiCheck className="service__list-icon"/> 
-              <p> I improve an already existing application or website visually and functionally by using better designs and modern technology. </p>
+              <p> I improve an already existing application or website visually and  
+                functionally by using better designs and modern technology. 
+              </p>
             </li>
           </ul>
         </articles>
@@ -42,24 +44,6 @@ const Services = () => {
             </li>
           </ul>
         </articles>
-
-        {/* <articles className="service">
-          <div className="service__head">
-            <h3>Technical Writing</h3>
-          </div>
-          <ul className="service__list">
-            <li>
-              <BiCheck className="service__list-icon"/> 
-              <p>I make technical jargons easily understandable to a layman.</p>
-            </li>
-
-            <li>
-              <BiCheck className="service__list-icon"/> 
-              <p>I build Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam nam nulla magnam. </p>
-            </li>
-          </ul>
-        </articles> */}
-
 
       </div>
     </section>
